8e8d8837094c338b6c58f48caeaa4614a7462929
aribilgiogr <aribilgiogr@gmail.com>
2026-07-01 21:06:51 +0300

    Update 02 01.07.2026

---
+++ Business/Middlewares/GlobalExceptionHandling.cs
@@ -0,0 +1,36 @@
+﻿using Core.Concretes.Models;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace Business.Middlewares
+{
+    public class GlobalExceptionHandling
+    {
+        private readonly RequestDelegate next;
+
+        public GlobalExceptionHandling(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var response = new Reply
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+        }
+    }
+}
---
+++ Business/Middlewares/IoC.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business.Middlewares
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Register your business services here
+            return services;
+        }
+
+        public static IApplicationBuilder UseBusinessServices(this IApplicationBuilder app)
+        {
+            // Configure your business services here
+            return app;
+        }
+    }
+}
